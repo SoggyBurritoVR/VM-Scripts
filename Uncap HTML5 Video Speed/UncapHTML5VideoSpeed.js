85e21389f8fd2d3a6d74ad2390b523534977183a
SoggyBurrito <69495940+SoggyBurritoVR@users.noreply.github.com>
2026-01-12 23:32:43 -0500

    Update UncapHTML5VideoSpeed.js

--- Uncap HTML5 Video Speed/UncapHTML5VideoSpeed.js
+++ Uncap HTML5 Video Speed/UncapHTML5VideoSpeed.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Uncap HTML5 Video Speed & Fast Complete
 // @namespace    https://github.com/SoggyBurritoVR/VM-Scripts/tree/main/Uncap%20HTML5%20Video%20Speed
-// @version      1.0
+// @version      1.1
 // @description  Remove playbackRate limits and allow speeds beyond 2x, plus hotkey to instantly finish video
 // @author       SB
 // @match        *://*/*
